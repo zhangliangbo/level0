85aedd2d709145ba869497d7040f08d623f4925e
zhangliangbo <2472110501@qq.com>
2020-06-18 18:30:52 +0800

    HostLookup PingTime

--- src/main/java/xxl/Main.java
+++ src/main/java/xxl/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
-        System.err.println(PingTime.pingTime("www.xxlun.com", 1));
+        System.err.println(PingTime.pingTime("www.xxlun.com"));
     }
 }
