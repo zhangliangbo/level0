5931c0783a1693c5c53595ddcf049d5cfb6a8263
zhangliangbo <2472110501@qq.com>
2020-06-24 14:16:15 +0800

    PopulateObject

--- src/main/java/xxl/mathematica/map/PopulateObject.java
+++ src/main/java/xxl/mathematica/map/PopulateObject.java
@@ -46,4 +46,49 @@ public class PopulateObject {
             return t;
         }).getOrNull();
     }
+
+    /**
+     * 把一个对象的同名字段放入另外一个字段
+     *
+     * @param src
+     * @param t
+     * @param <T>
+     * @return
+     */
+    public static <T> T populateObject(T src, T t) {
+        return Try.ofCallable(() -> {
+            Map<String, Object> map = Association.association(src);
+            Field[] fields = t.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+                field.set(t, map.get(field.getName()));
+            }
+            return t;
+        }).getOrNull();
+    }
+
+    /**
+     * 把一个对象的同名字段放入另外一个字段
+     *
+     * @param src
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> T populateObject(T src, Class<T> cls) {
+        return Try.ofCallable(() -> {
+            Map<String, Object> map = Association.association(src);
+            T t = cls.getDeclaredConstructor().newInstance();
+            Field[] fields = cls.getDeclaredFields();
+            for (Field field : fields) {
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+                field.set(t, map.get(field.getName()));
+            }
+            return t;
+        }).getOrNull();
+    }
 }
