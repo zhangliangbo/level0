9865b5a640c01248b28b546493d03bb5b525c709
zhangliangbo <2472110501@qq.com>
2020-07-07 21:48:35 +0800

    下载jar

--- src/test/groovy/xxl/mathematica/web/JarDownloadTest.groovy
+++ src/test/groovy/xxl/mathematica/web/JarDownloadTest.groovy
@@ -4,7 +4,7 @@ import java.util.function.BiConsumer
 
 class JarDownloadTest extends GroovyTestCase {
     void testJarDownload() {
-        println(JarDownload.jarDownload("io.netty:netty-all:4.1.50.Final", "C:\\Users\\Admin\\Desktop\\a", new BiConsumer<String, Float>() {
+        println(JarDownload.jarDownload("io.netty:netty-all:4.1.50.Final", "D:\\.gradle\\caches\\modules-2\\files-2.1", new BiConsumer<String, Float>() {
             @Override
             void accept(String s, Float aFloat) {
                 System.out.println(s + "\t" + aFloat)
