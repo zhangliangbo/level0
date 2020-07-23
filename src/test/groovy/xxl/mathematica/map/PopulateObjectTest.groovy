5036de035cb931f917b8cc7dc6815e131f19d7e8
zhangliangbo <2472110501@qq.com>
2020-07-23 13:38:17 +0800

    Values

--- src/test/groovy/xxl/mathematica/map/PopulateObjectTest.groovy
+++ src/test/groovy/xxl/mathematica/map/PopulateObjectTest.groovy
@@ -6,6 +6,10 @@ class PopulateObjectTest extends GroovyTestCase {
     void testPopulateObject() {
         def map = [name: "aaa", age: 1, info: "lllll", number: 5]
         println(PopulateObject.populateObject(map, Hello.class))
+    }
+
+    void testPopulateObject1() {
+        def map = [name: "aaa", age: 1, info: "lllll", number: 5]
         Hello hello = new Hello()
         println(PopulateObject.populateObject(map, hello))
     }
