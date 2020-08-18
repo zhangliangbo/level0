43044696a3f0f6cd9880a9f7693842c2bcf7ab82
zhangliangbo <2472110501@qq.com>
2020-08-18 19:50:13 +0800

    PrimerQ

--- src/main/java/xxl/mathematica/predication/PrimerQ.java
+++ src/main/java/xxl/mathematica/predication/PrimerQ.java
@@ -1,6 +1,7 @@
 package xxl.mathematica.predication;
 
 import io.vavr.control.Try;
+import org.apache.commons.math3.primes.Primes;
 
 /**
  * 素数判定
@@ -14,19 +15,6 @@ public class PrimerQ {
      * @return
      */
     public static boolean primerQ(int number) {
-        return Try.ofCallable(() -> {
-            boolean flag = true;
-            if (number < 2) {// 素数不小于2
-                return false;
-            } else {
-                for (int i = 2; i <= Math.sqrt(number); i++) {
-                    if (number % i == 0) {// 若能被整除，则说明不是素数，返回false
-                        flag = false;
-                        break;// 跳出循环
-                    }
-                }
-            }
-            return flag;
-        }).getOrNull();
+        return Try.ofCallable(() -> Primes.isPrime(number)).get();
     }
 }
