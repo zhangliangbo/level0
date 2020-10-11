313c11494aed878e6e505d6cef3bef275ad8b868
zhangliangbo <2472110501@qq.com>
2020-10-11 13:54:02 +0800

    Map转换Association

--- src/main/java/xxl/mathematica/java/Converter.java
+++ src/main/java/xxl/mathematica/java/Converter.java
@@ -1,7 +1,10 @@
 package xxl.mathematica.java;
 
 import xxl.mathematica.Rule;
+import xxl.mathematica.time.DateString;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -19,10 +22,19 @@ public class Converter {
      * @param map 字典
      * @return 列表
      */
-    public static <K, V> List<Rule<K, V>> map(Map<K, V> map) {
+    public static <K, V> List<Rule<Object, Object>> map(Map<K, V> map) {
         return io.vavr.collection.HashMap.ofAll(map)
                 .toList()
-                .map(tuple2 -> Rule.valueOf(tuple2._1(), tuple2._2()))
+                .map(tuple2 -> {
+                    Object key = tuple2._1();
+                    Object value = tuple2._2();
+                    if (tuple2._2() instanceof Date) {
+                        value = DateString.dateString((Date) tuple2._2());
+                    } else if (tuple2._2() instanceof Timestamp) {
+                        value = DateString.dateString((Timestamp) tuple2._2());
+                    }
+                    return Rule.valueOf(key, value);
+                })
                 .asJava();
     }
 }
