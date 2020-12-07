b5831de3c3cf8e50c5287c0514d75c54be811902
zhangliangbo <2472110501@qq.com>
2020-12-07 14:38:37 +0800

    使用entrySet()避免乱序

--- src/main/java/xxl/mathematica/java/Converter.java
+++ src/main/java/xxl/mathematica/java/Converter.java
@@ -4,9 +4,9 @@ import xxl.mathematica.Rule;
 import xxl.mathematica.time.DateString;
 
 import java.sql.Timestamp;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 把java类型转成mathematica类型
@@ -23,18 +23,23 @@ public class Converter {
      * @return 列表
      */
     public static <K, V> List<Rule<Object, Object>> map(Map<K, V> map) {
-        return io.vavr.collection.HashMap.ofAll(map)
-                .toList()
-                .map(tuple2 -> {
-                    Object key = tuple2._1();
-                    Object value = tuple2._2();
-                    if (tuple2._2() instanceof Date) {
-                        value = DateString.dateString((Date) tuple2._2());
-                    } else if (tuple2._2() instanceof Timestamp) {
-                        value = DateString.dateString((Timestamp) tuple2._2());
+        return Optional.ofNullable(map)
+                .orElse(new HashMap<>(1))
+                .entrySet()
+                .stream()
+                .map(new Function<Map.Entry<K, V>, Rule<Object, Object>>() {
+                    @Override
+                    public Rule<Object, Object> apply(Map.Entry<K, V> entry) {
+                        Object key = entry.getKey();
+                        Object value = entry.getValue();
+                        if (entry.getValue() instanceof Timestamp) {
+                            value = DateString.dateString((Timestamp) entry.getValue());
+                        } else if (entry.getValue() instanceof Date) {
+                            value = DateString.dateString((Date) entry.getValue());
+                        }
+                        return Rule.valueOf(key, value);
                     }
-                    return Rule.valueOf(key, value);
                 })
-                .asJava();
+                .collect(Collectors.toList());
     }
 }
