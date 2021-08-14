fb1e616b045afb3ddf9b7e16b402b0fba16e824f
zhangliangbo <2472110501@qq.com>
2021-08-14 12:12:37 +0800

    二维码

--- src/test/groovy/xxl/mathematica/image/BarcodeImageTest.groovy
+++ src/test/groovy/xxl/mathematica/image/BarcodeImageTest.groovy
@@ -2,9 +2,8 @@ package xxl.mathematica.image
 
 class BarcodeImageTest extends GroovyTestCase {
     void testBarcodeImage() {
-        String file = "C:\\Users\\EDZ\\Desktop\\tree.jpg"
-        println(BarcodeImage.barcodeImage("J6U5hMra", 500, file))
-        ShowImage.showImage(file)
+        String file = "C:\\Users\\EDZ\\Desktop\\b\\null.png"
+        println(BarcodeImage.barcodeImage(null, "K9GBMiYZ", 500, file))
     }
 
 }
